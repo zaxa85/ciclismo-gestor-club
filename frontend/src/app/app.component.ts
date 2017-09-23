52470abe535b4cfa525945f402e636a2b575216c
Pablo Sierralta <zaxa85@gmail.com>
2017-09-22 19:18:08 -0500

    Fixing style file, anyway need to improve it and new database dump

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['../styles.css']
-}) 
+  styleUrls: ['./styles.css']
+})
 export class AppComponent {
   title = 'Gestor de socios de clubes de ciclismo';
 }
